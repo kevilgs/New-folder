43e43774989e201e5442a39c02022a5bc645af2d
Kevil Shaji <kevilshaji@student.sfit.ac.in>
2024-10-22 09:06:08 +0530

    2nd commit

--- Main.java
+++ Main.java
@@ -3,5 +3,10 @@ class Main{
     public static void main(String []args){
         
         System.out.println("Hello Wolrd");
+        System.out.println("Hello Wolrd");
+        System.out.println("Hello Wolrd");
+        System.out.println("Hello Wolrd");
+        System.out.println("Hello Wolrd");
+        System.out.println("Hello Wolrd");
     }
 }
